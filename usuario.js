b5ade36f053e11d259159fd259311ffbe46301ca
fegarrucho281 <fegarrucho281@gmail.com>
2025-05-01 02:32:46 -0300

    Created class Usuario, and functions to sign up a new user, and sign in with it

--- usuario.js
+++ usuario.js
@@ -1,1 +1,40 @@
-console.log("Arquivo usuario.js rodando com sucesso!");
+class Usuario {
+
+    constructor() { // Construtor padrão
+
+        this.usuarios = []; // Irei salvar os usuários em um Array
+
+    }
+
+    cadastrarUsuario(nome, email, senha) {
+        
+        const usuarioExistente = this.usuarios.find(usuario => usuario.email === email); // O método find vai verificar se já existe 
+                                                                                        // esse usuário cadastrado no Array
+        if (usuarioExistente)
+            return "Email já cadastrado!";
+
+        const novoUsuario = { // Objeto para criar novo usuário e salvar no Array do construtor
+            nome,
+            email,
+            senha
+        }
+
+        this.push.usuarios(novoUsuario); // Colocando novo usuário no Array
+        return "Usuário cadastrado com sucesso!";
+
+    }
+
+    loginUsuario(email, senha) {
+
+        const usuario = this.usuarios.find(usuario => usuario.email === email);
+
+        if (!usuario)
+            return "Usuário não encontrado!"
+
+        if (usuario.senha !== senha)
+            return "Senha incorreta!"
+
+        return "Bem-vindo, ${usuario.nome}!"
+
+    }
+}
